8af6f18e47237fdb277f6d4b83f5809ced33e03f
Zheng Meyer-Zhao <zheng.meyer@gmail.com>
2015-09-10 16:15:47 +0800

    retrieve frequency setup infor for each antenna

--- autozoom.py
+++ autozoom.py
@@ -1,31 +1,41 @@
-# <AutoZoom: calculate ZoomFreqs automatically>                                      
-# Copyright (C) <2015> <Zheng Meyer-Zhao>                             
-#                                                                     
-# AutoZoom is free software: you can redistribute it and/or modify     
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or   
-# (at your option) any later version.                                 
-#                                                                     
-# AutoZoom is distributed in the hope that it will be useful,          
-# but WITHOUT ANY WARRANTY; without even the implied warranty of      
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       
-# GNU General Public License for more details.                        
-#                                                                     
-# You should have received a copy of the GNU General Public License   
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#########################################################################
+# <AutoZoom: calculate ZoomFreqs automatically>                         #             
+# Copyright (C) <2015> <Zheng Meyer-Zhao>                               #
+#                                                                       #
+# AutoZoom is free software: you can redistribute it and/or modify      #
+# it under the terms of the GNU General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or     #
+# (at your option) any later version.                                   #
+#                                                                       #
+# AutoZoom is distributed in the hope that it will be useful,           #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+# GNU General Public License for more details.                          #
+#                                                                       #
+# You should have received a copy of the GNU General Public License     #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>. #
+#########################################################################
 
 import vex
 
+# Retrieve information from frequency setup
+# Keys: side_band, num_channels, bandwidth, band_freqs, sample_rate
 def get_freqsetup(freq):
   f = {}
+  numchans = 0
   f['sample_rate'] = freq['sample_rate']
   f['side_band'] = freq['chan_def'][2]
   f['bandwidth'] = freq['chan_def'][3]
+  f['band_freqs'] = []
+  for ch in freq.getall('chan_def'):
+    numchans += 1
+    f['band_freqs'].append(ch[1])
+  f['num_channels'] = numchans
   return f
 
 # Dictionary antenna channels structure
-# {STATION: {MODE: {sample_rate: value}, {chan_def: value} } }
-
+# {STATION: {MODE: frequency_setup } }
+# see get_freqsetup for Keys in frequency_setup
 def get_antchannels(v):
   d = {}
   # loop through MODE, retrieve antenna FREQ setup
